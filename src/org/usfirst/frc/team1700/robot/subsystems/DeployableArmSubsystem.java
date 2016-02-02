fac143eab2a61758295dbb259795458e7918991a
KillerKK <katiemishra@Katies-MacBook-Pro.local>
2016-02-01 19:56:06 -0800

    Updated deployable arm code to include electronics and their associated methods.

--- src/org/usfirst/frc/team1700/robot/subsystems/DeployableArmSubsystem.java
+++ src/org/usfirst/frc/team1700/robot/subsystems/DeployableArmSubsystem.java
@@ -5,12 +5,17 @@ import org.usfirst.frc.team1700.robot.RobotMap;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DigitalInput;
 
+/* Initializes electronics associates with the deployable arm and 
+ * provides methods to control them.
+ */
 public class DeployableArmSubsystem {
 	DigitalInput frontLimitSwitch;
 	DigitalInput backLimitSwitch;
 	CANTalon armTalon;
 	double shooterDeadband;
 	
+	/* Constructor that initializes class variables and sets up armTalon
+	 * attributes. */
 	public DeployableArmSubsystem() {
 		backLimitSwitch = new DigitalInput (RobotMap.BACK_LIMIT_SWITCH);
 		frontLimitSwitch = new DigitalInput(RobotMap.FRONT_LIMIT_SWITCH);
@@ -18,40 +23,65 @@ public class DeployableArmSubsystem {
 		
 		armTalon = new CANTalon(RobotMap.ARM_TALON_ID);
 		armTalon.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+		armTalon.changeControlMode(CANTalon.TalonControlMode.Position);
+		armTalon.setPID(0,0,0);
 		armTalon.enableControl();
-		armTalon.changeControlMode(CANTalon.ControlMode.Speed);
 	}
 	
+	//Disables the armTalon 
 	public void disable() {
 		stop();
 		armTalon.disableControl();
 	}
 	
-	public void limitSwitchHit() {
-		stop();
-	}
-	
+	/* Returns boolean value if the arm is retracted, depending on 
+	 * if the limit switch is hit. */
 	public boolean isRetracted(){
-		if (armTalon.getEncPosition() < RobotMap.RETRACTED_ARM_POSITION + shooterDeadband && 
-			armTalon.getEncPosition() > RobotMap.RETRACTED_ARM_POSITION - shooterDeadband) {
-			return true;
-		} else {
-			return false;
-		}
+		return backLimitSwitch.get();
 	}
 	
-	public boolean isAtCollectorLevel() { // same deal as the other ones
-		return false;
+	/* Returns boolean value is the arm is at intake level, depending on
+	 * if it is in deadband range. */
+	public boolean isAtIntake() { 
+		if (armTalon.getEncPosition() < RobotMap.INTAKE_ARM_POSITION + shooterDeadband && 
+				armTalon.getEncPosition() > RobotMap.INTAKE_ARM_POSITION - shooterDeadband) {
+				return true;
+			} else {
+				return false;
+			}
 	}
 	
-	public boolean isAtPortcullisLevel(){
-		return false;	
+	/* Returns boolean value if the arm is at defense level, depending on 
+	 * if the limit switch is hit. */
+	public boolean isAtDefense() {
+		return frontLimitSwitch.get();
 	}	
 	
-	// stop the arm
+	// Stops the arm.
 	public void stop() {
 		armTalon.set(armTalon.getEncPosition()); //need to check if this is actually the current position
 	}
 	
+	/* While the back limit switch isn't hit, move to the retracted
+	 * position. */
+	public void goToRetracted() {
+		while (!backLimitSwitch.get()) {
+			armTalon.set(RobotMap.RETRACTED_ARM_POSITION);
+		}
+		stop();
+	}
 	
+	// Move to intake.
+	public void goToIntake() {
+		armTalon.set(RobotMap.INTAKE_ARM_POSITION);
+	}
+	
+	/* While the front limit switch isn't hit, move to the retracted
+	 * position. */
+	public void goToDefense() {
+		while (!frontLimitSwitch.get()) {
+			armTalon.set(RobotMap.DEFENSE_ARM_POSITION);
+		}
+		stop();		
+	}
 }
