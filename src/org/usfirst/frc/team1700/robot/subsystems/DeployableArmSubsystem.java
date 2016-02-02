39ac32bd9c8dcafac291791fc8da708473a49124
Vanessa Woo <vanessa@ProCatX.local>
2016-02-01 18:56:38 -0800

    New arm variables, cleaned up arm subsystem

--- src/org/usfirst/frc/team1700/robot/subsystems/DeployableArmSubsystem.java
+++ src/org/usfirst/frc/team1700/robot/subsystems/DeployableArmSubsystem.java
@@ -6,20 +6,20 @@ import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class DeployableArmSubsystem {
-	DigitalInput frontLimitSwitch = new DigitalInput(RobotMap.FRONT_LIMIT_SWITCH);
-	DigitalInput backLimitSwitch = new DigitalInput (RobotMap.BACK_LIMIT_SWITCH);
-	// DigitalInput armBeamBreakSensor = new DigitalInput (RobotMap.ARM_BEAM_BREAK_PORT);
-	CANTalon armTalon = new CANTalon(RobotMap.ARM_TALON_ID);
-	double shooterDeadband = 1.0;
-	double currentPosition = armTalon.getEncPosition();
+	DigitalInput frontLimitSwitch;
+	DigitalInput backLimitSwitch;
+	CANTalon armTalon;
+	double shooterDeadband;
 	
 	public DeployableArmSubsystem() {
+		backLimitSwitch = new DigitalInput (RobotMap.BACK_LIMIT_SWITCH);
+		frontLimitSwitch = new DigitalInput(RobotMap.FRONT_LIMIT_SWITCH);
+		shooterDeadband = 1.0;
+		
+		armTalon = new CANTalon(RobotMap.ARM_TALON_ID);
 		armTalon.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
-	}
-	
-	public void enable() {
-		//need to add get position and check if that really is what I assigned currentPosition to
 		armTalon.enableControl();
+		armTalon.changeControlMode(CANTalon.ControlMode.Speed);
 	}
 	
 	public void disable() {
@@ -30,9 +30,10 @@ public class DeployableArmSubsystem {
 	public void limitSwitchHit() {
 		stop();
 	}
-	public boolean isRetracted(double position){ // set a position for the retracted arm in robot map
-		if (currentPosition < position + shooterDeadband && 
-			currentPosition > position - shooterDeadband) { // probably have a deadband for this bc the arm's not always gonna be in the same place
+	
+	public boolean isRetracted(){
+		if (armTalon.getEncPosition() < RobotMap.RETRACTED_ARM_POSITION + shooterDeadband && 
+			armTalon.getEncPosition() > RobotMap.RETRACTED_ARM_POSITION - shooterDeadband) {
 			return true;
 		} else {
 			return false;
@@ -47,7 +48,10 @@ public class DeployableArmSubsystem {
 		return false;	
 	}	
 	
+	// stop the arm
 	public void stop() {
-		armTalon.set(currentPosition); //need to check if this is actually the current position
+		armTalon.set(armTalon.getEncPosition()); //need to check if this is actually the current position
 	}
+	
+	
 }
