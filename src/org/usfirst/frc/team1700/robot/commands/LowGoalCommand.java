7be2f01031e52ccc56cd67cf7d1219ece1f703a6
Katie Mishra <katiemishra@Katies-MacBook-Pro.local>
2016-02-03 19:44:26 -0800

    Finished low goal command.

--- src/org/usfirst/frc/team1700/robot/commands/LowGoalCommand.java
+++ src/org/usfirst/frc/team1700/robot/commands/LowGoalCommand.java
@@ -1,36 +1,45 @@
 package org.usfirst.frc.team1700.robot.commands;
 
+import org.usfirst.frc.team1700.robot.Subsystems;
+import org.usfirst.frc.team1700.robot.subsystems.IntakeSubsystem;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class LowGoalCommand extends Command {
+	
+	private IntakeSubsystem intake;
 
     public LowGoalCommand() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        requires(Subsystems.intake);
+        intake = Subsystems.intake;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	intake.backDrive();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	//nothing to do here, just automatically generated method
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return !intake.beamBreakBroken();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	intake.stopMotors();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	intake.stopMotors();
     }
 }
