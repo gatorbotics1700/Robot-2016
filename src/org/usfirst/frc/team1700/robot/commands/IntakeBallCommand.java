962a33ccbfad3374939abebd1c6a0256bc7c4796
Katie Mishra <katiemishra@Katies-MacBook-Pro.local>
2016-02-03 19:37:55 -0800

    Finished intake ball command.

--- src/org/usfirst/frc/team1700/robot/commands/IntakeBallCommand.java
+++ src/org/usfirst/frc/team1700/robot/commands/IntakeBallCommand.java
@@ -24,20 +24,22 @@ public class IntakeBallCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
+    	//nothing to do here, just automatically generated method
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	return intake.beamBreakBroken();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	intake.stopMotors();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	intake.stopMotors();
     }
 }
