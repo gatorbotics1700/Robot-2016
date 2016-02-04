6c1b0938821e73916e1a72d1502471c625cecd6e
eburnette1 <16eburnette@castilleja.org>
2016-02-03 19:09:28 -0800

    fixed an error

--- src/org/usfirst/frc/team1700/robot/subsystems/DeployableArmSubsystem.java
+++ src/org/usfirst/frc/team1700/robot/subsystems/DeployableArmSubsystem.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team1700.robot.subsystems;
 
 import org.usfirst.frc.team1700.robot.RobotMap;
+import org.usfirst.frc.team1700.robot.RobotUtils;
+
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
