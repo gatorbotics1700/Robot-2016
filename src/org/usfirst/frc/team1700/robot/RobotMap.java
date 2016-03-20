62aaee4767bfd9656002c41b8fe71368b9497202
gatorbotics <gatorbotics1700@gmail.com>
2016-03-20 15:37:30 -0700

    Update RobotMap.java
    Changed some buttons to put more control on the drive joystick

--- src/org/usfirst/frc/team1700/robot/RobotMap.java
+++ src/org/usfirst/frc/team1700/robot/RobotMap.java
@@ -14,19 +14,20 @@ public class RobotMap {
 						    THROTTLE = 5, // 5
 						    TANK_LEFT = 1,
 						    TANK_RIGHT = 5,
-							SHIFT_HIGH_BUTTON = 6,
+							 SHIFT_HIGH_BUTTON = 6,
 							SHIFT_LOW_BUTTON = 5,
-							OVERRIDE_BEAMBREAK_BUTTON_STOP = 3;
+							LOW_GOAL_BUTTON = 2,
+							NO_GRAVITY_BUTTON = 8;
+							
 							
 	// operating buttons
 	public static final int OPERATOR_JOYSTICK_PORT = 1,
 							SHOOT_BUTTON = 1,
-							//LOW_GOAL_BUTTON = 8, //I decided we don't need this and we need more buttons so this has to go
 							START_SHOOT_FAR_BUTTON = 5, //hood up
 							START_SHOOT_CLOSE_BUTTON = 4, //hood down
 							OVERRIDE_BEAMBREAK_BUTTON_INTAKE = 6,
 							OVERRIDE_BEAMBREAK_BUTTON_BACKDRIVE = 7,
-							NO_GRAVITY_BUTTON = 8,
+							OVERRIDE_BEAMBREAK_BUTTON_STOP = 8, 
 							//INTAKE_ARM_BUTTON = 10,
 							STRAIGHT_UP_ARM_BUTTON = 10,
 							DEFENSE_BUTTON = 9,
