187a8b03990696593ae29447d9873e8a0f008300
gatorbotics1700 <gatorbotics1700@gmail.com>
2016-04-26 23:44:38 -0700

    Autonomous drive over defense command ready to test!

--- src/org/usfirst/frc/team1700/robot/commands/Autonomous.java
+++ src/org/usfirst/frc/team1700/robot/commands/Autonomous.java
@@ -12,36 +12,39 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Autonomous extends CommandGroup {
 	private OI oi;
 	int autoMode = 6;
-	
+	int startingPos = 1; 
 	
     public Autonomous() {
 		this.oi = Robot.oi;
 	
     	if (autoMode == 1) { // this is for the moat
-        	addSequential(new AutonomousDriveForwardCommand(RobotMap.AUTO_FORWARD_DISTANCE));
+    		addSequential(new AutonomousDriveForwardCommand(RobotMap.AUTO_FORWARD_DISTANCE, false));
         	addSequential(new AutonomousDriveBackCommand(RobotMap.AUTO_BACKWARDS_DISTANCE));
     	} else if (autoMode == 2) { // this is just straight forward
-            addSequential(new AutonomousDriveForwardCommand(RobotMap.AUTO_FORWARD_DISTANCE));
+        	addSequential(new AutonomousDriveForwardCommand(RobotMap.AUTO_FORWARD_DISTANCE, false));
     	} else if (autoMode == 3) { // this is for the low bar
         	addSequential (new AutonomousArm(AutonomousArm.DESIRED_POSITION_DEFENSE));
-        	addSequential(new AutonomousDriveForwardCommand(RobotMap.AUTO_FORWARD_DISTANCE));
+        	addSequential(new AutonomousDriveForwardCommand(RobotMap.AUTO_FORWARD_DISTANCE, false));
         	addSequential (new AutonomousArm(AutonomousArm.DESIRED_POSITION_RETRACTED));
     	} else if (autoMode == 4) { // you're welcome Chris
-    		addSequential (new AutonomousDriveForwardCommand(RobotMap.CHEVAL_FRISE_ONE));
+    		addSequential (new AutonomousDriveForwardCommand(RobotMap.CHEVAL_FRISE_ONE, false));
     		addSequential (new AutonomousArm(AutonomousArm.DESIRED_POSITION_DEFENSE));
-    		addSequential (new AutonomousDriveForwardCommand(RobotMap.CHEVAL_FRISE_TWO));
+    		addSequential (new AutonomousDriveForwardCommand(RobotMap.CHEVAL_FRISE_TWO, false));
     		addSequential (new AutonomousArm(AutonomousArm.DESIRED_POSITION_RETRACTED));
-    		addSequential (new AutonomousDriveForwardCommand(RobotMap.CHEVAL_FRISE_THREE));    
+    		addSequential (new AutonomousDriveForwardCommand(RobotMap.CHEVAL_FRISE_THREE, false));    
     	} else if (autoMode == 5) { //you're welcome Natalie
-    		addSequential (new AutonomousDriveForwardCommand(RobotMap.SHOOT));
+    		addSequential (new AutonomousDriveForwardCommand(RobotMap.SHOOT, false));
     		addSequential (new AutonomousArm(AutonomousArm.DESIRED_POSITION_DEFENSE));
     		addSequential (new AutonomousShootHighGoalCommand());
     		addSequential (new AutonomousArm(AutonomousArm.DESIRED_POSITION_RETRACTED));
     		addSequential (new AutonomousDriveBackCommand(RobotMap.AUTO_BACKWARDS_DISTANCE));
     	} else if (autoMode == 6) {
     		addSequential (new AutonomousTurnToAngle(.2, 30));
+    	} else if (autoMode == 7) { //vision tracking autonomous command
+    		addSequential(new AutonomousDriveForwardCommand(RobotMap.AUTO_FORWARD_DISTANCE/3, false)); //the value should be the distance from starting position to front of defense... is probably not correct
+    		addSequential(new AutonomousDriveForwardCommand(RobotMap.AUTO_FORWARD_DISTANCE*2/3, true));
     	} else { // don't move bro
-    		new AutonomousDriveForwardCommand(0);
+    		new AutonomousDriveForwardCommand(0, false);
     	}
     }
 }
--- src/org/usfirst/frc/team1700/robot/commands/AutonomousDriveForwardCommand.java
+++ src/org/usfirst/frc/team1700/robot/commands/AutonomousDriveForwardCommand.java
@@ -5,7 +5,10 @@ import org.usfirst.frc.team1700.robot.Subsystems;
 import org.usfirst.frc.team1700.robot.subsystems.DeployableArmSubsystem;
 import org.usfirst.frc.team1700.robot.subsystems.DriveSubsystem;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.command.Command;
+
 /*
  * Our autonomous drive command
  * A distance is passed in from the autonomous command (can be a positive or negative distance)
@@ -14,33 +17,54 @@ import edu.wpi.first.wpilibj.command.Command;
 public class AutonomousDriveForwardCommand extends Command {
 	DriveSubsystem drive;
 	double autoDistance;
+	boolean useGyro;
+	private AHRS navX;
+	int counter;
+	double flatIntervalDeadband;
+	boolean goingOverDefense;
+	// Value obtained by dividing anticipated delay time by RoboRio loop delay time
+	private static final double DELAY_TIME_VALUE = 10;
 	
-	public AutonomousDriveForwardCommand (double autoDistance) {
+	public AutonomousDriveForwardCommand(double autoDistance, boolean useGyro) {
 		requires(Subsystems.drive);
 	    drive = Subsystems.drive;
 		this.autoDistance = autoDistance;
+		this.useGyro = useGyro; 
+		counter = 0; 
+		flatIntervalDeadband = 2/100;
+		goingOverDefense = false;
 	}
-	
-	
+
 	protected void initialize() {
 		drive.zeroEncoders();
 		drive.ShiftLow();
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	protected void execute() {
-		drive.ShiftLow();
-			drive.driveTank(-RobotMap.AUTO_SPEED, -RobotMap.AUTO_SPEED);
-			
-
-		
+		if (useGyro) accelDrive();
+		else regularDrive();		
 	} 
 
+	private void accelDrive() {
+		//navXValue = 9.81 (gravity) + rawAccelZ (nominally, -9.81)
+		double navXValue = 9.81+navX.getRawAccelZ();
+		regularDrive();
+		if (Math.abs(navXValue) > flatIntervalDeadband*9.81) {
+			counter = 0;
+		}else{
+			counter++;
+		}
+	}
+	
+	private void regularDrive() {
+		drive.ShiftLow();
+		drive.driveTank(-RobotMap.AUTO_SPEED, -RobotMap.AUTO_SPEED);
+	}
+	
 	@Override
 	protected boolean isFinished() {
-		if (Math.abs(drive.getRightDistance()) < autoDistance || Math.abs(drive.getLeftDistance()) < autoDistance) {
+		if ((counter < DELAY_TIME_VALUE) && (Math.abs(drive.getRightDistance()) < autoDistance || Math.abs(drive.getLeftDistance()) < autoDistance)) {
 			return false;
 		} else {
 			return true;
